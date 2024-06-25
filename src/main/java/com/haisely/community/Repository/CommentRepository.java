4d8fbc575af2ab406c3eff195d8cdc6ea5fe6ffe
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-06-25 14:30:00 +0900

    feat: comment repository interface

--- src/main/java/com/haisely/community/Repository/CommentRepository.java
+++ src/main/java/com/haisely/community/Repository/CommentRepository.java
@@ -1,4 +1,12 @@
 package com.haisely.community.Repository;
 
+import com.haisely.community.Entity.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository {
+    Optional<Comment> findCommentById(int id);;
+
+    List<Comment> findCommentsByBoardId(int id);
 }
