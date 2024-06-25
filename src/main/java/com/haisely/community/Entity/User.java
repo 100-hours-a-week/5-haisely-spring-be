a6bf787b37766295045666aef3630d73bebb04d5
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-06-25 09:57:16 +0900

    feat: jdbc setting

--- src/main/java/com/haisely/community/Entity/User.java
+++ src/main/java/com/haisely/community/Entity/User.java
@@ -1,7 +1,30 @@
 package com.haisely.community.Entity;
 
 import jakarta.persistence.Entity;
-
-@Entity
-public class User {
-}
+//
+//
+//@Entity
+//@Table(name = "users")
+//public class Users {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//    @Column(name = "name", nullable = false)
+//    private String name;
+//
+//    @Column(name = "email", nullable = false)
+//    private String email;
+//
+//    @Column(name = "phone", nullable = false)
+//    private String phone;
+//
+//    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+//    private Department department;
+//
+//    // 기본 생성자
+//    public User() {}
+//
+//    // 기타 생성자 및 Setter 등..
+//}
