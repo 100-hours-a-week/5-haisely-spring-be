c2b9b95275a7d94ccdbdd65b9ad1f33b57f7afa4
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-06-25 14:24:53 +0900

    feat: user repository interface

--- src/main/java/com/haisely/community/Repository/UserRepository.java
+++ src/main/java/com/haisely/community/Repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.haisely.community.Repository;
 
+import com.haisely.community.Entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
 public interface UserRepository {
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(int id);
+
+    Optional<User> findUserByNickname(String nickname);
 }
