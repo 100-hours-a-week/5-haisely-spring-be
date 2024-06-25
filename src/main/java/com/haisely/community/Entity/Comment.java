96c0d5725bafaac437c23eef6b51aabc7c8dfc8e
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-06-25 11:42:34 +0900

    feat: comment, image entity

--- src/main/java/com/haisely/community/Entity/Comment.java
+++ src/main/java/com/haisely/community/Entity/Comment.java
@@ -1,7 +1,41 @@
 package com.haisely.community.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.sql.Timestamp;
+
+@Getter
+@Setter
 @Entity
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="board_id")
+    private Board board;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
 }
--- src/main/java/com/haisely/community/Entity/Image.java
+++ src/main/java/com/haisely/community/Entity/Image.java
@@ -1,7 +1,19 @@
 package com.haisely.community.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "images")
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "file_url", nullable = false)
+    private String fileUrl;
 }
